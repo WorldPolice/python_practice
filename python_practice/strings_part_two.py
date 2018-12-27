97ce6fece1996c6058b45fa4ed88bbda9ceecb50
Zach Reichert <cheftyree@gmail.com>
2018-12-26 16:06:42 -0800

    Add lesson strings part two

---
+++ python_practice/strings_part_two.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+def comma_separated_strings(my_list):
+    """In this challenge we will give you a list in the argument 'my_list'.
+    If all objects in the list are strings concatenate them together into a single string separated by a comma
+    and return that string.  If any element in the list is not a string return False.
+
+    Example:
+        ['hello', 'world']
+        'hello,world'
+
+        ['hello', 'world', {'not': 'string'}]
+        False
+    """
+
+
+def search_for_substrings_in_list(substring, list_of_stuff):
+    """In this challenge you will get a string in the argument 'substring'
+    and a list of stuff in the argument 'list_of_stuff'. You need to inspect the 'list_of_stuff' and remove any
+    element where substring is not a substring of the element in question.
+    """
+
+
+def is_palindrome(palindrome):
+    """In this challenge you need to return True if the argument 'palindrome' is a palindrome and
+    return False if it is not a palindrome.  The definition of a palindrome is any word or phrase that can be read the
+    same backwards or forwards.
+
+    Example palindrome:
+        'A Santa dog lived as a devil God at NASA.'
+    """
+
+
+def remove_trailing_whitespace_characters(string):
+    """In this challenge you need to remove and trailing whitespace characters from the argument 'string'
+    and return that string.
+    """
+
+
+def replace_with_your_name(your_name, madlib):
+    """In this challenge you will be givien a string in the argument 'madlib'
+    Inside this string you will see '<name>' please replace this with the contents of the
+    variable 'your_name'.
+    """
