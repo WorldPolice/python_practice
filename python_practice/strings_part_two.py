7677c3faf80e3a1ecf7d47f2a5732601d9e003fc
Zach Reichert <cheftyree@gmail.com>
2019-01-21 17:14:32 -0800

    Address feedback on PR

--- python_practice/strings_part_two.py
+++ python_practice/strings_part_two.py
@@ -1,37 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-def comma_separated_strings(my_list):
-    """In this challenge we will give you a list in the argument 'my_list'.
-    If all objects in the list are strings concatenate them together into a single string separated by a comma
-    and return that string.  If any element in the list is not a string return False.
-
-    Example:
-        ['hello', 'world']
-        'hello,world'
-
-        ['hello', 'world', {'not': 'string'}]
-        False
-    """
-
-
-def search_for_substrings_in_list(substring, list_of_stuff):
-    """In this challenge you will get a string in the argument 'substring'
-    and a list of stuff in the argument 'list_of_stuff'. You need to inspect the 'list_of_stuff' and remove any
-    element where substring is not a substring of the element in question.
-    """
-
-
-def is_palindrome(palindrome):
-    """In this challenge you need to return True if the argument 'palindrome' is a palindrome and
-    return False if it is not a palindrome.  The definition of a palindrome is any word or phrase that can be read the
-    same backwards or forwards.
-
-    Example palindrome:
-        'A Santa dog lived as a devil God at NASA.'
-    """
-
-
 def remove_trailing_whitespace_characters(string):
     """In this challenge you need to remove and trailing whitespace characters from the argument 'string'
     and return that string.
@@ -42,4 +11,35 @@ def replace_with_your_name(your_name, madlib):
     """In this challenge you will be givien a string in the argument 'madlib'
     Inside this string you will see '<name>' please replace this with the contents of the
     variable 'your_name'.
+    """
+
+
+def concat_list_of_strings(my_list):
+    """In this challenge we will give you a list in the argument 'my_list'.
+    If all objects in the list are strings concatenate them together into a single string
+    and return that string.  If any element in the list is not a string return an empty string.
+
+    Example:
+        ['hello', 'world']
+        'helloworld'
+
+        ['hello', 'world', {'not': 'string'}]
+        ''
+    """
+
+
+def search_for_substrings_in_list(substring, list_of_stuff):
+    """In this challenge you will get a string in the argument 'substring'
+    and a list of stuff in the argument 'list_of_stuff'. You need to inspect the 'list_of_stuff' and return a list
+    of all the items that have that substring.
+    """
+
+
+def is_palindrome(palindrome):
+    """In this challenge you need to return True if the argument 'palindrome' is a palindrome and
+    return False if it is not a palindrome.  The definition of a palindrome is any word or phrase that can be read the
+    same backwards or forwards. Case and punctuation should be ignored.
+
+    Example palindrome:
+        'A Santa dog lived as a devil God at NASA.'
     """
